0f9888e14efa313667d3b57be961a8caf1609f42
Od Zarref <ludwigvonmarcelo@gmail.com>
2020-10-02 16:21:59 -0300

    Biblioteca usada em para buscar em FreeGamesSteam mudada para Selenium

--- freegamesbot.py
+++ freegamesbot.py
@@ -2,52 +2,10 @@ import requests
 import tweepy
 import os
 from selenium.webdriver import Firefox
+from selenium.common.exceptions import NoSuchElementException
 from bs4 import BeautifulSoup
 from time import sleep
 from tokens import *
-
-class ProcurarFreeSteamKeys():
-    def __init__(self):
-        def logProjeto(self, textoLog):
-            with open('bot_log.txt', 'a') as log:
-                log.write(textoLog)
-
-        def pegarUltimoVistoFreeSteamKeys(self):
-            with open('ultimovisto_freesteamkeys.txt') as arquivo:
-                return arquivo.read()
-
-        def atualizarUltimoVistoFreeSteamKeys(self, ultimoVisto):
-            with open('ultimovisto_freesteamkeys.txt', 'w') as arquivo:
-                arquivo.write(ultimoVisto)
-
-        def acessarPaginaDoJogo(url):
-            paginaDoJogo = requests.get(url)
-            paginaDoJogoSopa = BeautifulSoup(paginaDoJogo.text, 'html.parser')
-            ancora = paginaDoJogoSopa.find('a', class_='zf-edit-button')
-            botaoJogo = paginaDoJogoSopa.find('a', class_='item-url custom_link_button')
-
-            if ancora.get('href')[7:] in botaoJogo.get('onclick'):
-                logProjeto(self, f'JOGO STEAM! + {ancora.get("href")}' + '\n')
-
-        self.pagina = requests.get('https://www.freesteamkeys.com/')
-        self.sopa = BeautifulSoup(self.pagina.text, 'html.parser')
-        self.articles = self.sopa.find(id='post-items')
-
-        for contador, article in enumerate(self.articles, start=0):
-            divPostThumbnail = article.find(class_='post-thumbnail')
-            try:
-                if article.get('id') == pegarUltimoVistoFreeSteamKeys(self):
-                    break
-
-                if not divPostThumbnail.find('div', class_='expire_stamp'):
-                    print(divPostThumbnail.find('a').get('href'))
-                    acessarPaginaDoJogo(divPostThumbnail.find('a').get('href'))
-            except: 
-                pass
-
-            if contador == 1:
-                print('ISTO OCORREU')
-                atualizarUltimoVistoFreeSteamKeys(self, article.get('id'))
 
 class SeleniumBusca:
     def __init__(self):
@@ -66,6 +24,7 @@ class SeleniumBusca:
 
         for jogo in jogos:
             self.browser.get(jogo)
+            sleep(2)
 
             try:
                 botoes = self.browser.find_elements_by_tag_name('button')
@@ -88,7 +47,7 @@ class SeleniumBusca:
                 nomeJogo = self.browser.title
 
                 if not verificarJogoNaLista(self, self.browser.current_url):
-                    jogosGratis.append([nomeJogo, self.browser.current_url, ateQuando, 'Epic Games Store'])
+                    jogosGratis.append([nomeJogo, self.browser.current_url, ateQuando, 'Epic Games Store', 'game'])
                     # print(nomeJogo + ' | ' + self.browser.current_url + ' | ' + 'Jogo Grátis!')
             except:
                 pass
@@ -96,6 +55,82 @@ class SeleniumBusca:
 
         return jogosGratis
     
+    def procurarFreeSteamKeys(self):
+        def verificarExpirado(self, divPostThumbnail):
+            try:
+                divPostThumbnail.find_element_by_class_name('expire_stamp')
+                return True
+            except:
+                return False
+                
+        def acessarAnalizarPaginaDoJogo(self, link, linksJogos):
+            def tratarLink(self, link):
+                linkTratado = ''
+                for contador, caractere in enumerate(link):
+                    if contador > 23:
+                        if caractere == "'": break
+                        linkTratado += caractere
+                
+                return linkTratado
+
+            self.browser.get(link)
+            sleep(2)
+            ancoras = self.browser.find_elements_by_tag_name('a')
+
+            for ancora in ancoras:
+                try:
+                    linkTratado = tratarLink(self, ancora.get_attribute('onclick'))
+                    if 'store.steampowered' in linkTratado:
+                        print(linkTratado + ' | Jogo Steam!')
+                        linksJogosSteam.append(linkTratado)
+                except NoSuchElementException:
+                    pass
+
+
+        def pegarUltimoVistoFreeSteamKeys(self):
+            with open('ultimovisto_freesteamkeys.txt') as arquivo:
+                return arquivo.read()
+        
+        def atualizarUltimoVistoFreeSteamKeys(self, ultimoVisto):
+            with open('ultimovisto_freesteamkeys.txt', 'w') as arquivo:
+                arquivo.write(ultimoVisto)
+
+        self.browser.get('https://www.freesteamkeys.com/')
+        sleep(2)
+        divPostItems = self.browser.find_element_by_id('post-items')
+        articles = divPostItems.find_elements_by_tag_name('article')
+        linksJogos = list()
+        linksJogosSteam = list()
+        contador = 0
+
+        for article in articles:
+            itemId = article.get_attribute('id')
+            print(itemId)
+
+            if itemId == pegarUltimoVistoFreeSteamKeys(self):
+                print('Nenhuma atualização no site.')
+                break
+
+            if not itemId == 'post-':
+                if contador == 0: atualizarUltimoVistoFreeSteamKeys(self, itemId)
+                try:
+                    divPostThumbnail = article.find_element_by_class_name('post-thumbnail')
+
+                    if not verificarExpirado(self, divPostThumbnail):
+                        link = divPostThumbnail.find_element_by_tag_name('a').get_attribute('href')
+                        linksJogos.append(link)
+                except NoSuchElementException:
+                    pass
+                except:
+                    print('OCORREU UMA EXCEÇÃO EM "def procurarSteamKeys()"')
+                contador += 1
+
+        for link in linksJogos:
+            sleep(1)
+            acessarAnalizarPaginaDoJogo(self, link, linksJogos)
+                
+        return linksJogosSteam
+
     def fecharNavegador(self):
         os.remove('geckodriver.log')
         self.browser.quit()
@@ -115,9 +150,16 @@ class TwitterBotClass():
                     data += caractere
             
             return data
+            
+        def criarTexto(self):
+            hashtags = f'#freegames #freegame #{dadosJogo[0].replace(" ", "").lower()} '
+            if 'epic' in dadosJogo[3].lower(): hashtags += '#epic #epicgames #pcgaming'
+            string = f'🎮A NEW {dadosJogo[4].upper()} IS FOR FREE!🎮\n{dadosJogo[0]} está de graça na {dadosJogo[3]}!\nVálido até {pegarData(dadosJogo[2])[:-5]}.\n{hashtags}\n{dadosJogo[1]}'
 
-        self.api.update_status(f'{dadosJogo[0]} está de graça na {dadosJogo[3]}!\nVálido até {pegarData(dadosJogo[2])[:-5]}.\n{dadosJogo[1]}')
-        print(f'{dadosJogo[0]} está de graça na {dadosJogo[3]}! {dadosJogo[1]}!\nVálido até {pegarData(dadosJogo[2])[:-5]}')
+            return string
+
+        # self.api.update_status(f'{dadosJogo[0]} está de graça na {dadosJogo[3]}!\nVálido até {pegarData(dadosJogo[2])[:-5]}.\n{dadosJogo[1]}')
+        print(criarTexto(self))
 
     def mandarMensagem(self):
         self.api.send_direct_message(minhaContaPrincipal, 'TESTE BEM SUCEDIDO')
@@ -132,20 +174,18 @@ def salvarJogoGratis(jogo):
 
 if __name__ == '__main__':
     while True:
-        # print('BUSCANDO!')
-        # procurarFreeSteamKeys = ProcurarFreeSteamKeys()
-        # print(procurarFreeSteamKeys)
+        print('BUSCANDO!')
         buscar = SeleniumBusca()
-        jogos = buscar.epicGamesStore()
-        buscar.fecharNavegador()
-        print('-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-')
+        # jogos = buscar.epicGamesStore()
+        # buscar.fecharNavegador()
+        # print('-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-')
 
-        twitterBot = TwitterBotClass()
-        twitterBot.mandarMensagem()
+        # twitterBot = TwitterBotClass()
+        # # twitterBot.mandarMensagem()
                            
-        for jogo in jogos:
-            salvarJogoGratis(jogo)
-            twitterBot.criarTweet(jogo)
+        # for jogo in jogos:
+        #     salvarJogoGratis(jogo)
+        #     twitterBot.criarTweet(jogo)
 
-        sleep(1000)
+        sleep(10)
         
